cccb60b64f29c81e1454fcd29f531688632f9455
Conor O'Mara <omarac@tcd.ie>
2020-11-04 11:45:35 +0000

    Adding work from Atom editor

--- fizzbuzz_service_spec.py
+++ fizzbuzz_service_spec.py
@@ -1,11 +1,13 @@
-#unit test class
+# unit test class
 import unittest
 # according to TDD -> RED-GREEN-BLUE.
 # fail our unit test, then implement our business case depending on criteria.
 # we fail it at the beginning.
-# as a developer (we do so much copy and paste) then by doing RGB, we will be sure our methods work
+# as a developer (we do so much copy and paste) then by doing RGB, we will be
+# sure our methods work
 # when they eventually pass.
 from fizzbuzz_service import FizzBuzzService
+
 
 class FizzBuzzServiceTest(unittest.TestCase):
     def test_firsttest(self):
@@ -24,7 +26,9 @@ class FizzBuzzServiceTest(unittest.TestCase):
         self.assertEqual('FizzBuzz', FizzBuzzService.validate(45))
         self.assertEqual('FizzBuzz', FizzBuzzService.validate(60))
 
-#this is green phase (passing). this is a good way to start as we are iteratively improving our code.
 
-#Blue phase we must now optimise.
-#this is working. so if we have error, we will now
+# this is green phase (passing). this is a good way to start as we are
+# iteratively improving our code.
+
+# Blue phase we must now optimise.
+# this is working. so if we have error, we will now
